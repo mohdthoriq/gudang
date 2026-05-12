418f2c5beba23452f557ef4320caf2387b1cf27d
mohdthoriq <mohdthoriq@gmail.com>
2026-05-12 13:56:50 +0700

    feat: initialize Prisma ORM and define core data schema with API scaffold

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,4 +1,3 @@
-import express, { type Application, type Request, type Response } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
@@ -7,18 +6,6 @@ import { successResponse } from "./utils/response.js";
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './utils/swagger.js';
 import { errorHandler } from "./middleware/errorHandler.middleware.js";
-import authRouter from "./modules/auth/auth.router.js";
-import divisionRouter from "./modules/divisions/division.route.js";
-import classRouter from "./modules/classes/class.route.js";
-import attendanceRouter from "./modules/attendance/attendance.route.js";
-import assignmentRouter from "./modules/assignments/assignment.route.js";
-import submissionRouter from "./modules/assignment-content/assignment-content.route.js";
-import userRouter from "./modules/users/user/user.route.js";
-import userProfileRouter from "./modules/users/profile/profile.route.js";
-import waliSantriRouter from "./modules/wali/profileWali/waliProfile.route.js";
-import relasiRouter from "./modules/wali/relasi/relation.route.js";
-import dailyJournalRouter from "./modules/dailyJourney/dailyJourney.route.js";
-import dailyScoreRouter from "./modules/score/score.route.js";
 
 interface CustomRequest extends Request {
   rawBody?: string;
@@ -58,18 +45,6 @@ app.get("/", (req: Request, res: Response) => {
     });
 });
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/divisions', divisionRouter)
-app.use('/api/v1/classes', classRouter)
-app.use('/api/v1/attendances', attendanceRouter)
-app.use('/api/v1/assignments', assignmentRouter)
-app.use('/api/v1/submissions', submissionRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/relasi', relasiRouter)
-app.use('/api/v1/user-profile', userProfileRouter)
-app.use('/api/v1/wali-santri', waliSantriRouter)
-app.use('/api/v1/daily-journal', dailyJournalRouter)
-app.use('/api/v1/monthly-evaluation', dailyScoreRouter)
 
 app.get(/.*/, (req: Request, res: Response) => {
   throw new Error(`Route ${req.originalUrl} tidak ada di API E-Commerce`);
